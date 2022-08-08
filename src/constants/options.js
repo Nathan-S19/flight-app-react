e357be301e4e738fea547ae84dd21642a638ef82
Nathan-S19 <87956209+Nathan-S19@users.noreply.github.com>
2022-08-08 21:23:25 +1000

    Delete src/constants directory

--- src/constants/options.js
+++
@@ -1,9 +0,0 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '02ab2283aamsheb4acb513ca858bp1f15abjsn5794f21b4cec',
-    'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com',
-  },
-};
-
-export default options;
